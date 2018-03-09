1b31c0cc275931b244d74a05dd0d09228c717aea
Jordi <ofineo@gmail.com>
2018-03-09 19:00:38 +0000

    shown simple google map

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
 
-ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(    
+        <App />, document.getElementById('root'));
 registerServiceWorker();
