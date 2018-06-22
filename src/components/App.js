08560c8fa2906cd40b3f96cb454c93f24500d9e4
Jordi <ofineo@gmail.com>
2018-06-22 23:13:55 +0100

    feat: markers data moved to an external file

--- src/components/App.js
+++ src/components/App.js
@@ -4,77 +4,11 @@ import './App.css';
 import MapContainer from './map/MapContainer'
 import SideBar from './sidebar/SideBar';
 import escapeRegExp from "escape-string-regexp";
+import markersData from '../data';
 
 class App extends Component {
   state = {
-    markers: [
-      {
-        "id": 0,
-        "position": {
-          "lat": 51.528462,
-          "lng": -0.154751
-        },
-        "title": "Secret Gardens",
-        "fourSquareId": "57d01d5638fa8331f862a9d8",
-        "animation": null,
-        "icon": google.maps.Marker.iconBase,
-      },
-      {
-        "id": 1,
-        "position": {
-          "lat": 51.5559573,
-          "lng": -0.1775898
-        },
-        "title": "Waterstones",
-        "fourSquareId": "4b5306b9f964a520ee8c27e3",
-        "animation": null,
-        "icon": google.maps.Marker.iconBase,
-      },
-      {
-        "id": 2,
-        "position": {
-          "lat": 51.571337,
-          "lng": -0.167617
-        },
-        "title": "Kenwood House",
-        "fourSquareId": "4ac518cef964a52034a620e3",
-        "animation": null,
-        "icon": google.maps.Marker.iconBase
-      },
-      {
-        "id": 3,
-        "position": {
-          "lat": 51.566927,
-          "lng": -0.147071
-        },
-        "title": "Highgate cementery",
-        "fourSquareId": "4ac518cef964a5201ba620e3",
-        "animation": null,
-        "icon": google.maps.Marker.iconBase,
-      },
-      {
-        "id": 4,
-        "position": {
-          "lat": 51.542414,
-          "lng": -0.148923
-        },
-        "title": "The Stables Market",
-        "fourSquareId": "4ac518ebf964a52049ac20e3",
-        "animation": null,
-        "icon": google.maps.Marker.iconBase,
-      },
-      {
-        "id": 5,
-        "position": {
-          "lat": 51.569902,
-          "lng": -0.173975
-        },
-        "title": "Spaniards Inn",
-        "fourSquareId": "4ac518baf964a520dfa120e3",
-        "animation": null,
-        "icon": google.maps.Marker.iconBase
-      }
-    ],
+    markers: markersData,
     markersShown: [],
     menuOpen: false,
     mapInfoWindow: {
